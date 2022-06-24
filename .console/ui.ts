f3f6ae337cb1ce96ae4cc4cdc15b0933de512730
Willian Arana <willcruzarana@gmail.com>
2022-06-24 02:19:05 -0300

    chore: create UI to print board

---
+++ .console/ui.ts
@@ -0,0 +1,20 @@
+import { info } from 'console';
+import { ChessPiece } from '../src/chess/domain/chess-piece';
+export class UI {
+  public static printBoard(pieces: ChessPiece[][]): void {
+    const size = pieces.length;
+    for (let i = 0; i < size; i++) {
+      info(8 - i, ' ');
+      for (let j = 0; j < size; j++) {
+        UI.printPiece(pieces[i][j]);
+      }
+      info('\n');
+    }
+    info('  a b c d e f g h');
+  }
+
+  public static printPiece(piece: ChessPiece | null): void {
+    !!piece ? info(piece) : info('-');
+    info(' ');
+  }
+}
