e93bbc69fc8075b1274e5d03e9efa45a4b38222b
Willian Arana <willcruzarana@gmail.com>
2022-06-24 02:18:13 -0300

    refactor: clean Piece

--- src/@shared/domain/piece.ts
+++ src/@shared/domain/piece.ts
@@ -3,13 +3,5 @@ import { Position } from './position';
 
 export abstract class Piece {
   protected position!: Position;
-  private readonly _board: BoardInterface;
-
-  constructor(board: BoardInterface) {
-    this._board = board;
-  }
-
-  get board(): BoardInterface {
-    return this._board;
-  }
+  constructor(public readonly board: BoardInterface) {}
 }
