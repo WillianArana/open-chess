cb35d63d6c3898ee21aa3effe28aff4e55638da8
Willian Arana <willcruzarana@gmail.com>
2022-07-21 01:11:20 -0300

    fix: bug when apply value in matrix

--- src/@shared/domain/helpers/create-matrix.ts
+++ src/@shared/domain/helpers/create-matrix.ts
@@ -1,5 +1,15 @@
 const createMatrix =
   <T>(i: number) =>
-  (j: number) =>
-    Array<T[]>(i).fill(Array<T>(j).fill(null as unknown as T));
+  (j: number) => {
+    const matrix: T[][] = [];
+    for (let x = 0; x < i; x++) {
+      const row: T[] = [];
+      for (let y = 0; y < j; y++) {
+        row.push(null as unknown as T);
+      }
+      matrix.push(row);
+    }
+    return matrix;
+  };
+
 export default createMatrix;
