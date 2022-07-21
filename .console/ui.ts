dbe8d201137b7129f1728310fd87cbbae2cb976c
Willian Arana <willcruzarana@gmail.com>
2022-07-21 01:07:00 -0300

    refactor: remove method printPiece

--- .console/ui.ts
+++ .console/ui.ts
@@ -5,17 +5,14 @@ export class UI {
   public static printBoard(pieces: ChessPiece[][]): void {
     const size = pieces.length;
     for (let i = 0; i < size; i++) {
-      info(8 - i, ' ');
+      let row = `${8 - i} `;
       for (let j = 0; j < size; j++) {
-        UI.printPiece(pieces[i][j]);
+        const piece = pieces[i][j] as ChessPiece | null;
+        row += !!piece ? `${piece}` : '-';
+        row += ' ';
       }
-      info('\n');
+      info(row);
     }
     info('  a b c d e f g h');
-  }
-
-  public static printPiece(piece: ChessPiece | null): void {
-    !!piece ? info(piece) : info('-');
-    info(' ');
   }
 }
