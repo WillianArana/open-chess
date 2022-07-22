5ec867f936e45664a2c370ce309d747f653bf9ec
Willian Arana <willcruzarana@gmail.com>
2022-07-22 02:37:52 -0300

    refactor: clean class position

--- src/board/domain/position.ts
+++ src/board/domain/position.ts
@@ -1,22 +1,8 @@
 export class Position {
-  private _row: number;
-  private _column: number;
-
-  constructor(row: number, column: number) {
-    this._row = row;
-    this._column = column;
-  }
-
-  get row(): number {
-    return this._row;
-  }
-
-  get column(): number {
-    return this._column;
-  }
+  constructor(public readonly row: number, public readonly column: number) {}
 
   //@Override
   public toString(): string {
-    return `${this._row}, ${this._column}`;
+    return `${this.row}, ${this.column}`;
   }
 }
