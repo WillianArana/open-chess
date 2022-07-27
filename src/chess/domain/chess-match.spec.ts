b160f9dcc3fc0b315db5fd6550458d1482a1c596
Willian Arana <willcruzarana@gmail.com>
2022-07-27 00:51:05 -0300

    chore: create perform chess move

--- src/chess/domain/chess-match.spec.ts
+++ src/chess/domain/chess-match.spec.ts
@@ -1,6 +1,8 @@
 import { ChessMatch } from './chess-match';
+import { ChessPosition } from './chess-position';
 
-describe('ChessMatch', () => {
+const entity = ChessMatch.name;
+describe(entity, () => {
   it('should be create "ChessMatch"', () => {
     expect(new ChessMatch()).toBeDefined();
   });
@@ -20,6 +22,26 @@ describe('ChessMatch', () => {
       expect(pieces[6].length).toBe(8);
       expect(pieces[7].length).toBe(8);
       expect(pieces[8]).toBeUndefined();
+    });
+  });
+
+  describe('performChessMove', () => {
+    it('should be perform chess move', () => {
+      const chessMatch = new ChessMatch();
+      const source = new ChessPosition('e', 1);
+      const target = new ChessPosition('e', 2);
+      const capturedPiece = chessMatch.performChessMove(source, target);
+      expect(capturedPiece).toEqual(null);
+    });
+
+    it('should throw error when made a invalid  move', () => {
+      const chessMatch = new ChessMatch();
+      const source = new ChessPosition('a', 3);
+      const target = new ChessPosition('a', 5);
+
+      expect(() => chessMatch.performChessMove(source, target)).toThrowError(
+        'There is no piece on source position'
+      );
     });
   });
 });
--- src/chess/domain/chess-match.ts
+++ src/chess/domain/chess-match.ts
@@ -1,9 +1,12 @@
 import { Board } from '@src/board/domain/board';
+import { Position } from '@src/board/domain/position';
 
 import createMatrix from '@shared/domain/helpers/create-matrix';
+import { Piece } from '@shared/domain/piece';
 
 import { ChessPiece } from './chess-piece';
 import { ChessPosition, Column, Row } from './chess-position';
+import { ChessError } from './chess.error';
 import { Color } from './color';
 import { King } from './pieces/king';
 import { Rook } from './pieces/rook';
@@ -51,5 +54,28 @@ export class ChessMatch {
   private placeNewPiece(column: Column, row: Row, piece: ChessPiece): void {
     const position = new ChessPosition(column, row).toPosition();
     this._board.placePiece(piece, position);
+  }
+
+  public performChessMove(
+    sourcePosition: ChessPosition,
+    targetPosition: ChessPosition
+  ): ChessPiece | null {
+    const source = sourcePosition.toPosition();
+    const target = targetPosition.toPosition();
+    this.validateSourcePosition(source);
+    return this.makeMove(source, target) as ChessPiece | null;
+  }
+
+  private validateSourcePosition(position: Position): void {
+    if (!this._board.thereIsAPiece(position)) {
+      throw new ChessError('There is no piece on source position');
+    }
+  }
+
+  private makeMove(source: Position, target: Position): Piece | null {
+    const piece = this._board.removePiece(source) as Piece;
+    const capturedPiece = this._board.removePiece(target);
+    this._board.placePiece(piece, target);
+    return capturedPiece;
   }
 }
