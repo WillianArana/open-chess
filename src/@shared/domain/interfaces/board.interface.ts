14b28a241293a3df3bc8b5a7d1e0f613d34bee30
Willian Arana <willcruzarana@gmail.com>
2022-07-29 02:43:44 -0300

    chore: add methods to board protocol

--- src/@shared/domain/interfaces/board.interface.ts
+++ src/@shared/domain/interfaces/board.interface.ts
@@ -2,5 +2,9 @@ import { Position } from '../../../board/domain/position';
 import { Piece } from '../piece';
 
 export interface BoardInterface {
+  readonly rows: number;
+  readonly columns: number;
   piece(position: Position): Piece | null;
+  positionExists(position: Position): boolean;
+  thereIsAPiece(position: Position): boolean;
 }
