b9023405fd6c984528d433811dcd0d823c617051
Willian Arana <willcruzarana@gmail.com>
2022-07-29 02:47:49 -0300

    chore: start possible moves and check if possible

--- src/@shared/domain/piece.spec.ts
+++ src/@shared/domain/piece.spec.ts
@@ -1,19 +1,41 @@
 import { Board } from '../../board/domain/board';
+import createMatrix from './helpers/create-matrix';
 import { Piece } from './piece';
 
-class PieceMock extends Piece {}
+class PieceMock extends Piece {
+  public possibleMoves(): boolean[][] {
+    return this.createMatrixPossibleMoves();
+  }
+}
+
+class FoolPieceMock extends PieceMock {
+  protected createMatrixPossibleMoves(): boolean[][] {
+    const { rows, columns } = this.board;
+    return createMatrix<boolean>(rows)(columns, true);
+  }
+}
 
 const entity = Piece.name;
 describe(entity, () => {
   it('should be create piece', () => {
-    const board = new Board(2, 3);
+    const board = new Board(2, 2);
     const piece = new PieceMock(board);
     expect(piece).toBeDefined();
   });
 
   it('should get board', () => {
-    const board = new Board(2, 3);
+    const board = new Board(3, 3);
     const piece = new PieceMock(board);
     expect(piece.board).toBeInstanceOf(Board);
+  });
+
+  describe('isThereAnyPossibleMove', () => {
+    it('should check if is there any possible move', () => {
+      const board = new Board(3, 3);
+      const piece = new PieceMock(board);
+      const chessPiece = new FoolPieceMock(board);
+      expect(piece.isThereAnyPossibleMove()).toBeFalsy();
+      expect(chessPiece.isThereAnyPossibleMove()).toBeTruthy();
+    });
   });
 });
--- src/@shared/domain/piece.ts
+++ src/@shared/domain/piece.ts
@@ -1,7 +1,31 @@
 import { Position } from '../../board/domain/position';
+import createMatrix from './helpers/create-matrix';
 import { BoardInterface } from './interfaces/board.interface';
 
 export abstract class Piece {
-  public position!: Position | null;
-  constructor(public readonly board: BoardInterface) {}
+  public position: Position | null;
+  constructor(public readonly board: BoardInterface) {
+    this.position = null;
+  }
+
+  public abstract possibleMoves(): boolean[][];
+
+  public possibleMove(position: Position): boolean {
+    return this.possibleMoves()[position.row][position.column];
+  }
+
+  public isThereAnyPossibleMove(): boolean {
+    const possibleMoves = this.possibleMoves();
+    for (let i = 0; i < possibleMoves.length; i++) {
+      for (let j = 0; j < possibleMoves.length; j++) {
+        if (possibleMoves[i][j]) return true;
+      }
+    }
+    return false;
+  }
+
+  protected createMatrixPossibleMoves(): boolean[][] {
+    const { rows, columns } = this.board;
+    return createMatrix<boolean>(rows)(columns, false);
+  }
 }
