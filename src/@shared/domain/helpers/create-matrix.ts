e5bf736704b6b6ee852688daa6dce8e16a56715d
Willian Arana <willcruzarana@gmail.com>
2022-07-29 02:41:39 -0300

    chore: add default value when start matrix

--- src/@shared/domain/helpers/create-matrix.ts
+++ src/@shared/domain/helpers/create-matrix.ts
@@ -1,11 +1,11 @@
 const createMatrix =
   <T>(i: number) =>
-  (j: number) => {
+  (j: number, startValue = null as unknown as T) => {
     const matrix: T[][] = [];
     for (let x = 0; x < i; x++) {
       const row: T[] = [];
       for (let y = 0; y < j; y++) {
-        row.push(null as unknown as T);
+        row.push(startValue);
       }
       matrix.push(row);
     }
