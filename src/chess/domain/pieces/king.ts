108d8c98e34dda82dbf7edf325ef3ea554da9caf
Willian Arana <willcruzarana@gmail.com>
2022-07-29 03:00:04 -0300

    chore: start make King possible moves

--- src/chess/domain/pieces/king.ts
+++ src/chess/domain/pieces/king.ts
@@ -2,6 +2,11 @@ import { ChessPiece } from '../chess-piece';
 
 export class King extends ChessPiece {
   //@Override
+  public possibleMoves(): boolean[][] {
+    return this.createMatrixPossibleMoves();
+  }
+
+  //@Override
   public toString(): string {
     return this.isWhite ? '♔' : '♚';
   }
